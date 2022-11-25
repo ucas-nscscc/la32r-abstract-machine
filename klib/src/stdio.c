7bb93243f2a98a8224ae9398fa502364ca7e0a24
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2022-11-25 23:46:25 +0800

    add stdio and string funcs and run nemu at batch mode

--- klib/src/stdio.c
+++ klib/src/stdio.c
@@ -5,24 +5,103 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+static int am_itoa(int i, char *buf)
+{
+	int neg = 0, len;
+	char *buf_begin = buf;
+	char temp;
+
+	if (i < 0) {
+		neg = 1;
+		i = -i;
+	}
+
+	do {
+		*buf++ = (i % 10) + '0';
+		i /= 10;
+	} while (i > 0);
+
+	if (neg == 1) {
+		*buf++ = '-';
+	}
+
+	*buf = '\0';
+	len = buf - buf_begin;
+	
+	for (buf--; buf > buf_begin; buf--, buf_begin++) {
+		temp = *buf;
+		*buf = *buf_begin;
+		*buf_begin = temp;
+	}
+
+	return len;
+}
+
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+	char buf[256];
+	int ret;
+	va_list ap;
+
+	va_start(ap, fmt);
+	ret = vsprintf(buf, fmt, ap);
+	va_end(ap);
+
+	putstr(buf);
+	return ret;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+	int ret = 0;
+	char buf[24], ch;
+	char *str = NULL;
+
+	while ((ch = *(fmt++))) {
+		int len = 1;
+		if (ch == '%') {
+			ch = *(fmt++);
+			switch (ch) {
+				case 'd':
+					len = am_itoa(va_arg(ap, int), buf);
+					strcpy(&out[ret], buf);
+					break;
+				case 'c':
+					str = va_arg(ap, char*);
+					out[ret] = str[0];
+					break;
+				case 's':
+					str = va_arg(ap, char*);
+					len = strlen(str);
+					strcpy(&out[ret], str);
+					break;
+				default:
+					break;
+			}
+		} else {
+			out[ret] = ch;
+		}
+		ret += len;
+	}
+	out[ret] = '\0';
+	return ret;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+	int ret;
+	va_list ap;
+	
+	va_start(ap, fmt);
+	ret = vsprintf(out, fmt, ap);
+	va_end(ap);
+
+	return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+	panic("Not implemented");
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+	panic("Not implemented");
 }
 
 #endif
--- klib/src/string.c
+++ klib/src/string.c
@@ -5,43 +5,110 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+	// assert(s != NULL);
+	size_t len = 0;
+	while (*s++) {
+		len++;
+	}
+	return len;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+	// assert(dst != NULL || src != NULL);
+	int i;
+	for (i = 0; src[i] != '\0'; i++) {
+		dst[i] = src[i];
+	}
+	dst[i] = '\0';
+	return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+	// assert(dst != NULL || src != NULL);
+	int i;
+	for (i = 0; i < n && src[i] != '\0'; i++) {
+		dst[i] = src[i];
+	}
+	dst[i] = '\0';
+	return dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+	// assert(dst != NULL || src != NULL);
+	char *temp = dst;
+	while (*temp != '\0') {
+		temp++;
+	}
+	while ((*temp++ = *src++) != '\0') {
+		;
+	}
+	return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+	// assert(s1 != NULL || s2 != NULL);
+	while (*s1 == *s2 && *s1 != '\0') {
+		s1++;
+		s2++;
+	}
+	return *s1 - *s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+	// assert(s1 != NULL || s2 != NULL);
+	while (*s1 == *s2 && *s1 != '\0' && --n) {
+		s1++;
+		s2++;
+	}
+	return *s1 - *s2;
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+	// assert(s != NULL);
+	char *mem = s;
+	for (int i = 0; i < n; i++)
+	{
+		mem[i] = c;
+	}
+	return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+	// assert(dst != NULL && src != NULL);
+	char* tmp_dest = (char*)dst;
+	const char* tmp_src = (const char*)src;
+
+	if (tmp_src < tmp_dest){
+		while (n--) {
+			*tmp_dest++ = *tmp_src++;
+		}    
+	} else if (tmp_src > tmp_dest) {
+		tmp_src += n - 1;
+		tmp_dest += n - 1;
+		while (n--) {
+			*tmp_dest-- = *tmp_src;
+		}
+	}
+	return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+	// assert(in != NULL && out != NULL);
+	char* tmp_out = (char*)out;
+	const char* tmp_in = (const char*)in;
+	while (n--) {
+		*tmp_out++ = *tmp_in++;
+	}
+	return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+	// assert(s1 != NULL || s2 != NULL);
+	while (*(char*)s1 == *(char*)s2 && --n) {
+		s1 = (char*)s1 + 1;
+		s2 = (char*)s2 + 1;
+	}
+	return *(char*)s1 - *(char*)s2;
 }
 
 #endif
